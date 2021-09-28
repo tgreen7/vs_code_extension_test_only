afd38e9dae5b3f4ed218026a3aaebd15c07f34d0
Taoh Green <taohgreen@gmail.com>
2021-09-28 14:36:55 -0700

    Always remove other onlys before inserting new one

--- extension.js
+++ extension.js
@@ -14,10 +14,11 @@ function activate(context) {
   // The commandId parameter must match the command field in package.json
   const addOnlyCommand = vscode.commands.registerCommand(
     "test-only.addOnly",
-    function () {
+    async function () {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
+
       try {
         const editor = vscode.window.activeTextEditor;
         function tryToInsert(lineNumberToCheck) {
@@ -28,12 +29,14 @@ function activate(context) {
             const hasItOnly = activeText.match(/^\s*it\.only\(/);
             if (hasIt || hasItOnly) {
               editor.edit((editBuilder) => {
-                let from = "it(";
-                let to = "it.only(";
+                const from = "it(";
+                const to = "it.only(";
+
+                // first remove other onlys
+                removeOnlyHandler(editBuilder);
                 if (hasItOnly) {
-                  const tmp = from;
-                  from = to;
-                  to = tmp;
+                  // will have been removed along with others
+                  return;
                 }
                 const newLine = activeText.replace(from, to);
                 const range = new vscode.Range(
@@ -68,56 +71,62 @@ function activate(context) {
 
   const removeOnly = vscode.commands.registerCommand(
     "test-only.removeOnly",
-    function () {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      try {
-        const editor = vscode.window.activeTextEditor;
-
-        const linesToEdit = [];
-        for (
-          let lineNumber = 0;
-          lineNumber < editor.document.lineCount;
-          lineNumber++
-        ) {
-          const activeLine = editor.document.lineAt(lineNumber);
-          const activeText = activeLine.text;
-          const hasItOnly = activeText.includes("it.only(");
-
-          if (hasItOnly) {
-            linesToEdit.push(lineNumber);
-          }
-        }
-        if (linesToEdit.length) {
-          editor.edit((editBuilder) => {
-            linesToEdit.forEach((lineNumber) => {
-              const activeLine = editor.document.lineAt(lineNumber);
-              const activeText = activeLine.text;
-              const newLine = activeText.replace("it.only(", "it(");
-              const range = new vscode.Range(
-                lineNumber,
-                0,
-                lineNumber,
-                activeText.length
-              );
-              try {
-                editBuilder.replace(range, newLine);
-              } catch (error) {
-                console.error("error:", error);
-              }
-            });
-          });
-        }
-      } catch (error) {
-        console.error("error:", error);
-        vscode.window.showWarningMessage("Error removing onlys!");
-      }
-    }
+    () => removeOnlyHandler()
   );
   context.subscriptions.push(removeOnly);
 }
 
+async function removeOnlyHandler(_editBuilder) {
+  // The code you place here will be executed every time your command is executed
+
+  // Display a message box to the user
+  try {
+    const editor = vscode.window.activeTextEditor;
+
+    const linesToEdit = [];
+    for (
+      let lineNumber = 0;
+      lineNumber < editor.document.lineCount;
+      lineNumber++
+    ) {
+      const activeLine = editor.document.lineAt(lineNumber);
+      const activeText = activeLine.text;
+      const hasItOnly = activeText.includes("it.only(");
+
+      if (hasItOnly) {
+        linesToEdit.push(lineNumber);
+      }
+    }
+    if (linesToEdit.length) {
+      const cleanOnly = (editBuilder) => {
+        linesToEdit.forEach((lineNumber) => {
+          const activeLine = editor.document.lineAt(lineNumber);
+          const activeText = activeLine.text;
+          const newLine = activeText.replace("it.only(", "it(");
+          const range = new vscode.Range(
+            lineNumber,
+            0,
+            lineNumber,
+            activeText.length
+          );
+          try {
+            editBuilder.replace(range, newLine);
+          } catch (error) {
+            console.error("error:", error);
+          }
+        });
+      };
+      if (_editBuilder) {
+        cleanOnly(_editBuilder);
+      } else {
+        editor.edit(cleanOnly);
+      }
+    }
+  } catch (error) {
+    console.error("error:", error);
+    vscode.window.showWarningMessage("Error removing onlys!");
+  }
+}
 // this method is called when your extension is deactivated
 function deactivate() {}
 
